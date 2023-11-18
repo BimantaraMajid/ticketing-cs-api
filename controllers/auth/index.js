7f3ee6310f9c8c837be79c07ef25a5b32f02c7e5
majid <bimantarasc@gmail.com>
2023-11-18 16:48:09 +0700

    wrap with try-catch

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,27 +1,31 @@
 const bcrypt = require('bcrypt');
 const Users = require('./Users');
-const { httpSuccess, httpUnauthorized } = require('../../Utils/htttp-response');
+const { httpSuccess, httpUnauthorized, httpInternalServerError } = require('../../Utils/htttp-response');
 const { generateToken } = require('./token');
 
 const userAdmin = new Users();
 
 /** @type {import('express').Router} */
-const postLogin = (req, res) => {
-  const { username, password } = req.body;
+const postLogin = async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
-  const user = userAdmin.data.find((u) => u.username === username);
+    const user = userAdmin.data.find((u) => u.username === username);
 
-  if (!user || !bcrypt.compareSync(password, user.password)) {
-    return httpUnauthorized(res, 'Invalid username or password');
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return httpUnauthorized(res, 'Invalid username or password');
+    }
+
+    const token = generateToken(username);
+
+    return httpSuccess(res, {
+      id: user.id,
+      username: user.username,
+      token,
+    });
+  } catch (error) {
+    return httpInternalServerError(res);
   }
-
-  const token = generateToken(username);
-
-  return httpSuccess(res, {
-    id: user.id,
-    username: user.username,
-    token,
-  });
 };
 
 module.exports = {
